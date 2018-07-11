59754df98e33271c74f30422c57eca85f52ae894
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-10 23:56:27 -0400

    notes.js updated with crud functions

--- routes/notes.js
+++ routes/notes.js
@@ -1,50 +1,147 @@
 'use strict';
 
 const express = require('express');
-
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+const Note = require('../models/note');
 const router = express.Router();
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  mongoose.connect(MONGODB_URI)
+    .then(() => {
+      const searchTerm = req.query.searchTerm;
+      let filter = {};
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
+      if (searchTerm) {
+        filter.$or = [
+          { title: { $regex: searchTerm } },
+          { content: { $regex: searchTerm } }
+        ];
+      }
 
+      return Note.find(filter).sort({ updatedAt: 'desc' });
+    })
+    .then(results => {
+      if (results) {
+        res.json(results);
+      } else {
+        next();
+      }
+    })
+    .then(() => {
+      return mongoose.disconnect();
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
+  mongoose.connect(MONGODB_URI)
+    .then(() => {
+      const id = req.params.id;
 
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+      return Note.findById(id);
+    })
+    .then(results => {
+      if (results) {
+        res.json(results);
+      } else {
+        next();
+      }
+    })
+    .then(() => {
+      return mongoose.disconnect();
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
+  mongoose.connect(MONGODB_URI)
+    .then(() => {
+      const newNote = {
+        title: req.body.title,
+        content: req.body.content
+      };
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+      /***** Never trust users - validate input *****/
+      if (!newNote.title) {
+        const err = new Error('Missing `title` in request body');
+        err.status = 400;
+        return next(err);
+      }
 
+      return Note.create(newNote);
+    })
+    .then(response => {
+      res.location(`http://${req.headers.host}/notes/${response.id}`).status(201).json(response);
+    })
+    .then(() => {
+      return mongoose.disconnect();
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
+  mongoose.connect(MONGODB_URI)
+    .then(() => {
+      const id = req.params.id;
+      const updateObj = {
+        title: req.body.title,
+        content: req.body.content
+      };
+      
+      /***** Never trust users - validate input *****/
+      if (!updateObj.title) {
+        const err = new Error('Missing `title` in request body');
+        err.status = 400;
+        return next(err);
+      }
 
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
+      const options = { new: true };
 
+      return Note.findByIdAndUpdate(id, updateObj, options);
+    })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .then(() => {
+      return mongoose.disconnect();
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
+  mongoose.connect(MONGODB_URI)
+    .then(() => {
+      const id = req.params.id;
 
-  console.log('Delete a Note');
-  res.status(204).end();
+      return Note.findByIdAndRemove(id);
+    })
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .then(() => {
+      return mongoose.disconnect();
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
