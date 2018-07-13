7c2ca4a7d50cf375dc3c1937ea8d6298c17b577d
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-13 01:46:42 -0400

    post folders route complete

--- routes/folders.js
+++ routes/folders.js
@@ -6,7 +6,7 @@ const { MONGODB_URI } = require('../config');
 const Folder = require('../models/folder');
 const router = express.Router();
 
-// GET ALL FOLDERS
+// GET ALL /FOLDERS
 router.get('/', (req, res, next) => {
   const searchTerm = req.query.searchTerm;
   let filter = {};
@@ -30,12 +30,12 @@ router.get('/', (req, res, next) => {
     });
 });
 
-// GET A SINGLE FOLDER BY ID
+// GET /FOLDERS BY ID
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
 
   if(!(mongoose.Types.ObjectId.isValid(id))) {
-    const message = 'these are not the droids you are looking for';
+    const message = 'not a valid Mongo ObjectId';
     console.error(message);
     return res.status(404).send(message);
   }
@@ -47,6 +47,28 @@ router.get('/:id', (req, res, next) => {
       } else {
         next();
       }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// POST /FOLDERS TO CREATE A NEW FOLDER
+router.post('/', (req, res, next) => {
+  const newFolder = {
+    name: req.body.name
+  };
+
+  // VALIDATE USER INPUT
+  if(!newFolder.name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  Folder.create(newFolder)
+    .then(response => {
+      res.location(`http://${req.headers.host}/folders/${response.id}`).status(201).json(response);
     })
     .catch(err => {
       next(err);
